bc4c70d5917d135c66a3b1ef84669d9396d4f11c
kazuhisanoguchi <pasuwaado135@gmail.com>
2020-05-01 22:26:52 +0300

    Assignment7 Kazuhisa Noguchi FinalVersion

--- app/src/main/java/com/ljuv/fibonacciapp/activities/MainActivity.java
+++ app/src/main/java/com/ljuv/fibonacciapp/activities/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ljuv.fibonacciapp.R;
+import com.ljuv.fibonacciapp.utils.FibonacciNumbersCalculator;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +27,9 @@ public class MainActivity extends AppCompatActivity {
         final EditText fibonacciSeq = findViewById(R.id.tv_fibonacci_sequence);
 
         okButton.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
                 if(!fibonacciSeq.getText().toString().isEmpty()) {
-                    Intent intent = new Intent(context, FizzBuzzActivity.class).putExtra("nums", Integer.parseInt( fibonacciSeq.getText().toString()) );
+                    Intent intent = new Intent(context, FibonacciListActivity.class).putExtra("nums", Integer.parseInt( fibonacciSeq.getText().toString()) );
                     startActivity(intent);
                 }
             }
